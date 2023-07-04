c40e04a1d5992d4ece19469fcb4e233d7d057fcf
Kevin Patrick Don <137464580+kevpatdon@users.noreply.github.com>
2023-07-04 11:33:26 +0800

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -2,11 +2,14 @@ import os
 from PIL import Image
 import numpy as np
 
-def process_input(image, model):
-    img = Image.open(image)
-    img = img.resize((100, 100))
-    img = img.convert("L")
-    img = np.array(img)
-    img = img.flatten()
-    prediction = model.predict([img])
-    return prediction
+def process_input(image_path, model):
+    try:
+        img = Image.open(image_path)
+        img = img.resize((100, 100))
+        img = img.convert("L")
+        img = np.array(img)
+        img = img.flatten()
+        prediction = model.predict([img])
+        return prediction
+    except (OSError, IOError):
+        raise ValueError("Invalid image file or unable to open the image.")
