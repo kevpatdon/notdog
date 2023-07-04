ab3e9af725ea828a7dab08eccd608aefa8a47ae6
Kevin Patrick Don <137464580+kevpatdon@users.noreply.github.com>
2023-07-04 10:29:55 +0800

    Update predictor_model.py

--- predictor_model.py
+++ predictor_model.py
@@ -1,7 +1,8 @@
 import os
 from PIL import Image
 import numpy as np
-from sklearn.svm import OneClassSVM
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
 
 def get_image_paths(folder_path):
     image_paths = []
@@ -11,11 +12,12 @@ def get_image_paths(folder_path):
     return image_paths
 
 data_folder = "Training_Data"
-subfolders = ["dogs"]
+subfolders = ["dogs", "not_dogs"]
 
 def load_model():
     X_train = []  # Reset X_train list for each model loading
-    for subfolder in subfolders:
+    y_train = []
+    for label, subfolder in enumerate(subfolders):
         subfolder_path = os.path.join(data_folder, subfolder)
         image_paths = get_image_paths(subfolder_path)
         for image_path in image_paths:
@@ -25,7 +27,22 @@ def load_model():
             img = np.array(img)
             img = img.flatten()
             X_train.append(img)
+            y_train.append(label)
     X_train = np.array(X_train)
-    model = OneClassSVM(nu=0.2)
-    model.fit(X_train)
+    y_train = np.array(y_train)
+
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+    dtrain = xgb.DMatrix(X_train, label=y_train)
+    dval = xgb.DMatrix(X_val, label=y_val)
+
+    params = {
+        "objective": "binary:logistic",
+        "eval_metric": "logloss",
+        "eta": 0.3,
+        "max_depth": 3
+    }
+
+    model = xgb.train(params, dtrain, num_boost_round=100, evals=[(dval, "Validation")])
+
     return model
