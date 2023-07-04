f7655e0a39dfdc96731f82dfa8199d486e062052
Kevin Patrick Don <137464580+kevpatdon@users.noreply.github.com>
2023-07-04 13:51:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,36 @@
 import os
 from PIL import Image
 import streamlit as st
-from prediction import process_input
-from predictor_model import load_model
+from predictor import process_input
+from predictor_model import train_model
 
 def main():
     st.title("DOG? NOT DOG?")
     st.write("Upload an image and the app will determine whether this is an image of a dog.")
 
-    uploaded_files = st.file_uploader("Choose images", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+    uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
-    if uploaded_files is not None:
-        dog_count = 4000
-        not_dog_count = 4000
+    if uploaded_file is not None:
+        file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
+        # st.write(file_details)
 
-        model = load_model()  # Load the model
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
 
-        os.makedirs("uploads", exist_ok=True)  # Create the "uploads" directory
+        if st.button('Is that a dog?'):
+            # Create the 'uploads' directory if it doesn't exist
+            os.makedirs('uploads', exist_ok=True)
 
-        for uploaded_file in uploaded_files:
-            dog_count += 1
-            not_dog_count += 1
+            image_path = os.path.join('uploads', uploaded_file.name)
+            image.save(image_path)
 
-            if dog_count > 4010 or not_dog_count > 4010:
-                break
+            model = train_model()  # Load the model
+            prediction = process_input(image_path, model)
 
-            file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
-
-            image_path = os.path.join("uploads", uploaded_file.name)
-            with open(image_path, "wb") as f:
-                f.write(uploaded_file.getbuffer())
-
-            image = Image.open(image_path)
-            st.image(image, caption='Uploaded Image', use_column_width=True)
-
-            if st.button('Is that a dog?'):
-                prediction = process_input(image_path, model)
-
-                if prediction == "dogs":
-                    st.image('dog.jpg', caption='That is a dog.')
-
-                    correct = st.button('Correct')
-                    if not correct:
-                        dogs_folder = "Training_Data/dogs"
-                        new_filename = f"dog.{dog_count}"
-                        new_image_path = os.path.join(dogs_folder, new_filename + ".jpg")
-                        image.save(new_image_path)
-                else:
-                    st.image('notdog.jpg', caption='That is not a dog.')
-
-                    correct = st.button('Correct')
-                    if not correct:
-                        not_dogs_folder = "Training_Data/not_dogs"
-                        new_filename = f"cat.{not_dog_count}"
-                        new_image_path = os.path.join(not_dogs_folder, new_filename + ".jpg")
-                        image.save(new_image_path)
-
-            # Remove the uploaded file after processing
-            os.remove(image_path)
-
-
+            if prediction == 1:
+                st.image('dog.jpg', caption='That is a dog.')
+            else:
+                st.image('notdog.jpg', caption='That is not a dog.')
 
 if __name__ == '__main__':
     main()
